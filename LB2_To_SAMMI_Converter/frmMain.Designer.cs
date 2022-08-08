2fc516d3ba26a088b82f9be3bdb3a8296ad9c7cb
Chrizzz_1508 <christian.s1508@web.de>
2022-08-08 16:40:25 +0200

    Projektdateien hinzufügen.

---
+++ LB2_To_SAMMI_Converter/frmMain.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace LB2_To_SAMMI_Converter
+{
+    partial class ExtensionConverter
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExtensionConverter));
+            this.rtbLB2 = new System.Windows.Forms.RichTextBox();
+            this.rtbSEF = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // rtbLB2
+            // 
+            this.rtbLB2.Location = new System.Drawing.Point(12, 136);
+            this.rtbLB2.Name = "rtbLB2";
+            this.rtbLB2.Size = new System.Drawing.Size(529, 398);
+            this.rtbLB2.TabIndex = 2;
+            this.rtbLB2.Text = "";
+            // 
+            // rtbSEF
+            // 
+            this.rtbSEF.Location = new System.Drawing.Point(643, 136);
+            this.rtbSEF.Name = "rtbSEF";
+            this.rtbSEF.Size = new System.Drawing.Size(529, 398);
+            this.rtbSEF.TabIndex = 4;
+            this.rtbSEF.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 39);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "LB2 Extension (.lb2)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.label2.Location = new System.Drawing.Point(712, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(382, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "SAMMI Extension (.sef)";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.btnSave.Location = new System.Drawing.Point(643, 551);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(529, 74);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save SEF file";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.btnLoad.Location = new System.Drawing.Point(12, 551);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(529, 74);
+            this.btnLoad.TabIndex = 5;
+            this.btnLoad.Text = "Load LB2 file";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(552, 136);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(76, 397);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = ">\r\n>\r\n>\r\n>\r\n>\r\n";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F);
+            this.txtFileName.Location = new System.Drawing.Point(293, 28);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(879, 47);
+            this.txtFileName.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(5, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(282, 39);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Extension Name:";
+            // 
+            // ExtensionConverter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 641);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbSEF);
+            this.Controls.Add(this.rtbLB2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "ExtensionConverter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Extension Converter LB2 => SAMMI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbLB2;
+        private System.Windows.Forms.RichTextBox rtbSEF;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ LB2_To_SAMMI_Converter/frmMain.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace LB2_To_SAMMI_Converter
+{
+    public partial class ExtensionConverter : Form
+    {
+        string sFilePath = "";
+        public ExtensionConverter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Please select your .lb2 file";
+            ofd.Filter = "LB2 Extension|*.lb2";
+            ofd.Multiselect = false;
+
+            if (DialogResult.OK == ofd.ShowDialog())
+            {
+                using (StreamReader sr = new StreamReader(ofd.FileName,true))
+                {
+                    rtbLB2.Text = sr.ReadToEnd();
+                    txtFileName.Text = ofd.SafeFileName.Replace(".lb2","");
+
+                    sr.Close();
+                }
+                sFilePath = ofd.FileName.Replace(ofd.SafeFileName, "").Replace(".lb2","");
+                btnConvert_Click(null, null);
+            }
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string s = rtbLB2.Text;
+            s = s.Replace("LioranBoardJSON.", "SAMMIJSON.");
+
+            Regex reg = new Regex("\"lioranboard_version\":\"(.{4,15})\"\\W");
+            Match m = reg.Match(s);
+            if (m.Success)
+            {
+                s = s.Replace(m.Groups[1].Value, "2022.4.0");
+            }
+
+            s = s.Replace("\"lioranboard_version\":", "\"sammi_version\":");
+
+            s = s.Replace("lioranboardclient.send(", "sammiclient.send(");
+            s = s.Replace("LB.method(", "SAMMI.method(");
+            s = s.Replace("LB.getVariable(", "SAMMI.getVariable(");
+            s = s.Replace("LB.setVariable(", "SAMMI.setVariable(");
+            s = s.Replace("LB.deleteVariable(", "SAMMI.deleteVariable(");
+            s = s.Replace("LB.insertArray(", "SAMMI.insertArray(");
+            s = s.Replace("LB.deleteArray(", "SAMMI.deleteArray(");
+            s = s.Replace("LB.extCommand(", "SAMMI.extCommand(");
+            s = s.Replace("LB.triggerExt(", "SAMMI.triggerExt(");
+            s = s.Replace("LB.triggerButton(", "SAMMI.triggerButton(");
+            s = s.Replace("LB.modifyButton(", "SAMMI.modifyButton(");
+            s = s.Replace("LB.popUp(", "SAMMI.popUp(");
+            s = s.Replace("LB.alert(", "SAMMI.alert(");
+            s = s.Replace("LB.notification(", "SAMMI.notification(");
+            s = s.Replace("LB.getDeckList(", "SAMMI.getDeckList(");
+            s = s.Replace("LB.getDeck(", "SAMMI.getDeck(");
+            s = s.Replace("LB.getImage(", "SAMMI.getImage(");
+            s = s.Replace("LB.getSum(", "SAMMI.getSum(");
+            s = s.Replace("LB.stayInformed(", "SAMMI.stayInformed(");
+            s = s.Replace("LB.getActiveButtons(", "SAMMI.getActiveButtons(");
+            s = s.Replace("LB.getModifiedButtons(", "SAMMI.getModifiedButtons(");
+            s = s.Replace("LB.getTwitchList(", "SAMMI.getTwitchList(");
+            s = s.Replace("LB.trigger(", "SAMMI.trigger(");
+            s = s.Replace("LB.close(", "SAMMI.close(");
+            s = s.Replace("lioranboard.on(", "sammiclient.on(");
+            s = s.Replace("lioranboardclient.addListener(", "sammiclient.addListener(");
+            s = s.Replace("lioranboardclient.removeListener(", "sammiclient.removeListener(");
+            rtbSEF.Text = s;
+
+            if(sender == null)
+            {
+                btnSave_Click(null, null);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(rtbSEF.Text) || String.IsNullOrEmpty(txtFileName.Text))
+            {
+                MessageBox.Show("Not all fields were filled out correctly. Save process aborted!");
+            }
+            if (String.IsNullOrEmpty(sFilePath))
+            {
+                sFilePath = AppDomain.CurrentDomain.BaseDirectory;
+            }
+            using (StreamWriter sw = new StreamWriter(sFilePath + txtFileName.Text + ".sef"))
+            {
+                sw.Write(rtbSEF.Text.Replace("\r\n", "\n").Replace("\r", "\n").Replace("\n", "\r\n"));
+                sw.Flush();
+                sw.Close();
+            }
+            if (sender != null)
+            {
+                if (DialogResult.Yes == MessageBox.Show("Extension " + txtFileName.Text + " was created in " + sFilePath + ". Open the folder now?", "Open folder?", MessageBoxButtons.YesNo))
+                {
+                    Process.Start(sFilePath);
+                }
+            }
+        }
+    }
+}
